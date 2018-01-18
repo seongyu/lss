0a826af840de764a9c2366b97ea6a47981fde15b
LeonKim <leonkim@LeonKimui-MacBook-Pro-4.local>
2018-01-18 15:04:26 +0900

    tt

--- cassandra/lora_streaming/models.py
+++ cassandra/lora_streaming/models.py
@@ -4,7 +4,7 @@ from cassandra.cqlengine.models import Model
 class Store00(Model):
 	pid = columns.Text(partition_key=True)
 	tms = columns.DateTime(primary_key=True, clustering_order='DESC')
-	cid = columns.Integer(required=True)
+	cid = columns.Text(required=True)
 	fid = columns.Integer(required=True)
 	rcid = columns.Text(required=True)
 	sdid = columns.Text(required=True)
