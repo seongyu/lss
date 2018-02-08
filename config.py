5a73c7550345c7f30d2a976e11fdaf0d45696883
LeonKim <leonkim@LeonKimui-MacBook-Pro-4.local>
2018-02-08 15:05:50 +0900

    update schema

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ if DTP == 'S':
 	MQ_HOST = 'localhost'
 	CA_HOST = '10.140.0.8' #for cassandra name, Use Internal IP
 elif DTP == 'T': #TestMode
-	MQ_HOST = '35.201.132.176' # for develop computer
+	# MQ_HOST = '35.201.132.176' # for develop computer
 	MQ_HOST = 'localhost' # for GCP server
-	# CA_HOST = '10.140.0.8' 
+	CA_HOST = '10.140.0.8' 
 else : #DevelopMode
 	CA_HOST = 'localhost'
 	MQ_HOST = 'localhost'
